6c366bf5c147dc2f20713fa66f630b704a7d1032
vivekfreshworks <vivek.kumar2@freshworks.com>
2024-11-28 14:47:07 +0530

    add hello api

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => {
   res.send('Hello, Cloud Run!');
 });
+app.get('/hello', (req, res) => {
+    res.send('Hello, Cloud Run! hello boy');
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
